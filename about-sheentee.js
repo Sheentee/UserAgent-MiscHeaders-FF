85ea4892612e0df16e39b46f9b04be77690cff2c
Barry Keegan <barry.keegan@gmail.com>
2026-05-02 20:29:35 +0100

    refactor: clean up duplicate HTML head tags and remove extraneous donation CSS classes from buttons

--- about-sheentee.js
+++ about-sheentee.js
@@ -3,11 +3,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     const footer = document.querySelector('.footer.fixed-footer');
     if (footer) {
-        footer.innerHTML = `
-            <div class="footer-info">
-                Get more information about Sheentee Extensions 
-                <a href="https://github.com/Sheentee/.github/blob/main/README.md" target="_blank">Here</a>
-            </div>
-        `;
+        const div = document.createElement('div');
+        div.className = 'footer-info';
+        div.textContent = 'Get more information about Sheentee Extensions ';
+        
+        const a = document.createElement('a');
+        a.href = 'https://github.com/Sheentee/.github/blob/main/README.md';
+        a.target = '_blank';
+        a.textContent = 'Here';
+        
+        div.appendChild(a);
+        footer.appendChild(div);
     }
 });
